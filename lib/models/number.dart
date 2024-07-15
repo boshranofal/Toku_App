c2358d4c0147a72f716f55a134652f3415ec2154
boshranofal <boshranofal66@gmail.com>
2024-07-15 14:02:41 +0300

    :)

---
+++ lib/models/number.dart
@@ -0,0 +1,7 @@
+class Number {
+  final String enName;
+  final String jpName;
+  final String image;
+  const Number(
+      {required this.enName, required this.jpName, required this.image});
+}
--- lib/screens/numbers_page.dart
+++ lib/screens/numbers_page.dart
@@ -1,61 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:language_learning_app/components/item.dart';
+import 'package:language_learning_app/models/number.dart';
 
 class NumbersPage extends StatelessWidget {
   const NumbersPage({super.key});
-  final Number one = const Number(
-      enName: 'one',
-      jpName: 'ichi',
-      image: 'assets/images/number/number_one.png');
+  final List<Number> numbers = const [
+    Number(
+        enName: 'one',
+        jpName: 'ichi',
+        image: 'assets/images/number/number_one.png'),
+    Number(
+        enName: 'two',
+        jpName: 'ni',
+        image: 'assets/images/number/number_two.png'),
+    Number(
+        enName: 'three',
+        jpName: 'san',
+        image: 'assets/images/number/number_three.png'),
+    Number(
+        enName: 'four',
+        jpName: 'shi',
+        image: 'assets/images/number/number_four.png'),
+    Number(
+        enName: 'five',
+        jpName: 'go',
+        image: 'assets/images/number/number_five.png'),
+    Number(
+        enName: 'six',
+        jpName: 'roku',
+        image: 'assets/images/number/number_six.png'),
+    Number(
+        enName: 'seven',
+        jpName: 'shichi',
+        image: 'assets/images/number/number_seven.png'),
+    Number(
+        enName: 'eight',
+        jpName: 'hachi',
+        image: 'assets/images/number/number_eight.png'),
+    Number(
+        enName: 'nine',
+        jpName: 'kyu',
+        image: 'assets/images/number/number_nine.png'),
+    Number(
+        enName: 'ten',
+        jpName: 'ju',
+        image: 'assets/images/number/number_ten.png'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xff46322B),
-        title: const Text(
-          "Numbers",
-          style: TextStyle(color: Colors.white),
+        appBar: AppBar(
+          backgroundColor: const Color(0xff46322B),
+          title: const Text(
+            "Numbers",
+            style: TextStyle(color: Colors.white),
+          ),
         ),
-      ),
-      body: Container(
-        height: 100,
-        color: const Color(0xffEF9235),
-        child: Row(
-          children: [
-            Container(
-              color: Colors.white,
-              child: Image.asset('assets/images/number/number_one.png',
-                  width: 100, height: 100),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 12.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'ichi',
-                    style: TextStyle(fontSize: 18, color: Colors.white),
-                  ),
-                  Text('One',
-                      style: TextStyle(fontSize: 18, color: Colors.white)),
-                ],
-              ),
-            ),
-            const Spacer(),
-            const Padding(
-              padding: EdgeInsets.only(right: 8.0),
-              child: Icon(Icons.play_arrow, size: 32, color: Colors.white),
-            )
-          ],
-        ),
-      ),
-    );
+        body: ListView.builder(
+            itemCount: numbers.length,
+            itemBuilder: (context, index) {
+              return Item(number: numbers[index]);
+            }));
   }
-}
-
-class Number {
-  final String enName;
-  final String jpName;
-  final String image;
-  const Number(
-      {required this.enName, required this.jpName, required this.image});
 }
